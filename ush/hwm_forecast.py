4ebc3a895a35c710f6cd7729510b23fbcd44cd4a
FloydFayton-NCO <floyd.fayton@noaa.gov>
2023-10-19 17:19:06 -0500

    missing logic for time dependent changes but modifying existing models seems to be working, written to new.json in definition.

--- ush/hwm_forecast.py
+++ ush/hwm_forecast.py
@@ -1,4 +1,4 @@
-"""hwm_forecast.py: predictive json files for HWM DAILY and LIVE charts"""
+"""hwm_forecast.py: predictive json files for HWM charts"""
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 __author__ = 'Floyd Fayton'
 __credits__ = ["Floyd Fayton"]
@@ -10,57 +10,59 @@ __status__ = "test"
 import readline
 import sys
 import json
-import time
+import re
 from datetime import datetime, timedelta, date
 import os
 import numpy as np
 import pandas as pd
 
 
-def test(jsonfile,ctrl):
-    with open(jsonfile,'r') as file:
-        data = json.load(file)
+def hwm_modify(jsonfile,ctrl):
+    with open(jsonfile,'r') as jfile:
+        data = json.load(jfile)
 
-    with open(ctrl,'r') as file:
-        jsonctrl = pd.read_csv(file,sep=" ",comment="#",header=None,skip_blank_lines=True,names=['sign','model','number','times'])
+    with open(ctrl,'r') as cfile:
+        jsonctrl = pd.read_csv(cfile,sep=" ",comment="#",header=None,skip_blank_lines=True,names=['sign','model','number','times'])
         jsonctrl = jsonctrl.fillna("0") # no NaNs or empty fields, last two fields in control have to be integers
 
     for idx, cow in jsonctrl.iterrows():
         sign=cow['sign']
         model=cow['model']
         number=int(cow['number'])
-        times=cow['times']
-        
+        times=cow['times'] #not integrated yet
+
         #addition, multiplicatio, division, or subtraction
         for row in data:
-            if model in row[0]['name']:
-                print(model)
-                for i, _ in enumerate(row):
-                    big=row[i]['data'][:][-1]
-                    x1=row[i]['data'][:][-1][1]
+            modelfound=re.fullmatch(model, row[0]['name'])
+            if modelfound:
+                for i in range(len(row[0]['data'][:])):
+                    x1=row[0]['data'][:][i][-1]
                     x2=number
-                    print(x1," ",sign," ",x2,"=") # math test FAFJ
+                    # print(row[0]['data'][:][i][-1])
+                    # print(x1," ",sign," ",x2,"=") # math test FAFJ
                     if sign == '*': # sign * 0 (number) == 0
                         nuvalue=max(0,int(x1*x2))
-                        row[i]['data'][:][-1][1]=nuvalue
+                        row[0]['data'][:][i][-1]=nuvalue
                     elif sign == '-': # double meaning if number is 0 or empty, should both result in zero? FAFJ
                         if number == 0:
                             x1=0
                             continue
                         else:
                             nuvalue=max(0,int(x1-x2))
-                            row[i]['data'][:][-1][1]=nuvalue
+                            row[0]['data'][:][i][-1]=nuvalue
                     elif sign == '/':
                         nuvalue=max(0,int(x1/x2))
-                        row[i]['data'][:][-1][1]=nuvalue
+                        row[0]['data'][:][i][-1]=nuvalue
                     elif sign == '+':
                         nuvalue=max(0,int(x1+x2))
-                        row[i]['data'][:][-1][1]=nuvalue
+                        row[0]['data'][:][i][-1]=nuvalue
                     else:
                         print('operand not detected, please fix $PARMhwm/fcst_ctrl file')
                         continue
-                    result=row[i]['data'][:][-1][1]
-                    print(result,"\n") # math test FAFJ
+                    result=row[0]['data'][:][i][-1]
+                    # print(x1," ",sign," ",x2,"=",result,"\n") # math test FAFJ
+    with open('new.json','w') as final:
+        final.write(json.dumps(data,indent=4,sort_keys=False))
 
 ctrlfile = 'parm/fcst_ctrl'
 infile = 'cactus_daily_nid_nodes_p1.json'
@@ -70,7 +72,7 @@ infile = 'cactus_daily_nid_nodes_p1.json'
 # fcst_ctrl = os.environ["FCST_CTRL"]
 # workdir = os.environ["DATA"] + '/'
 
-test(infile,ctrlfile)
+hwm_modify(infile,ctrlfile)
 
 
 
